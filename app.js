a53dff59f212a7d40cdd8b13f0107cdc2ae83e56
rohail <rupani.rohail@gmail.com>
2023-05-19 23:48:12 +0500

    message

---
+++ app.js
@@ -0,0 +1,447 @@
+// chap 1
+// alert("rohail")
+// alert("rizwan")
+// alert("abc@gmail.com")
+// alert("123-456-789")
+// var password="123456"
+// alert(password)
+// alert("You're learning JavaScript!")
+// alert ("have a nice day")
+
+// chap2
+
+// var fullName ="Rohail"
+// var lastName
+// lastName="rizwan"
+// console.log(lastName)
+
+// var teamName="pakistan"
+// alert(teamName)
+
+// var bestMan = "Charlie";
+// bestMan="john"
+// console.log(bestMan)
+
+// chap 3
+// var caseQty
+// caseQty=144
+// var num=9 //use as a operation
+
+// var sum
+// sum=2+2
+// console.log(sum)
+
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// console.log(orderTotal)
+
+// var num=5
+// num=5+5
+// console.log(num)
+
+//chap4
+
+var product_cost = 3.45;
+var nameofBand;
+
+var price=50
+
+var rohailRizwan;
+
+//legal variable
+// firstName
+// _privateVariable
+// $price
+// camelCase
+// PI
+
+//illegal variable
+// 123abc 
+// my-variable 
+// first name 
+// var 
+// function
+
+//chap5
+// 1) modulus
+//  var num = 20 % 6;
+//  console.log(num) //2
+
+// var largeNum=1000*2000
+// console.log(largeNum)
+
+// var num1=15
+// var num2 = 10
+// var diff= num1-num2
+// console.log(diff)
+
+// var num = 25;
+// var divisor = 3;
+
+// var remainder = num % divisor;
+// console.log(remainder);
+
+// var num1=15
+// var num2 = 10
+// var product= num1*num2
+// alert(product)
+
+// chap 6
+
+// x+=1
+// var x=100
+// x--
+// console.log(x)
+
+// var x = 50;
+// var y = x++;
+// console.log(y)
+
+// var y = 50;
+// var z = --y;
+// console.log(z)
+
+// var num=5
+// var newNum=--num
+// console.log(newNum)
+
+// var num=5
+// var newNum=++num
+// console.log(newNum)
+
+// var num=50;
+// num++
+// var newNum=num
+// console.log(newNum)
+
+//chap7
+// var calculatedNum = 2 + (2 * 6);
+// console.log(calculatedNum)
+// var calculatedNum = (2 + 2) * 6;
+// console.log(calculatedNum)
+// var calculatedNum = (2 + 2) * (4 + 2);
+// console.log(calculatedNum)
+// var calculatedNum = ((2 + 2) * 4) + 2;
+// console.log(calculatedNum)
+
+// var result = (2 + 2) * (4 + 10)
+// console.log(result)
+// var result = 2 + (2 * 4 )+ 10
+// console.log(result)
+// var result = (4 / 2 )* 4
+// console.log(result)
+
+// var num = "2" + "2";
+// console.log(num)
+// message = ("Hello," + "Dolly");
+// alert(message)
+
+// alert("33" + 3);
+// alert("Pakistan"+"Zindabad")
+
+// var concat="rohail"+2003
+// console.log(concat)
+
+// var f_name="rohail"
+// var l_name="rizwan"
+// var fullName=f_name+l_name
+// console.log(fullName)
+
+// var name=prompt("Enter first name:")
+// var country=prompt("enter country","china")
+// var yourName = prompt("Enter Your Name");
+
+// var remarks=prompt("greeting","good")
+
+// var num1="number"
+// var value=5
+// var display=prompt(num1,value)
+
+// var name=prompt("Name")
+// var display=name
+// alert(display)
+
+// chap10
+// var city = "Karachi"
+// if (city = "Karachi") {
+// console.log("The City OF Lights")
+// }
+
+// var x=5,y=5
+// var z,value
+// if(x===5){
+//   z=+prompt("enter number")
+//   value=z
+//   console.log(value)
+// }
+
+// var zipcode=+prompt("zipcode?")
+// if(zipcode=10010){
+//     alert("karachi")
+// }
+// else{
+//     alert("Please write correct city")
+// }
+
+// var x=5
+
+// if(x==5){
+//     x=1
+//     console.log(x)
+// }
+
+// chap 11
+
+// var num=4
+// if(num!=3){
+//     console.log(num)
+// }
+
+// if(num1>=num2)
+
+// var num=10
+// if(num!=11){
+//     num=11
+//     console.log(num)
+// }
+
+// var y_name=prompt("enter your name")
+
+// var anotherName="rizwan"
+
+// if(y_name!=anotherName){
+//     alert("no match")
+// }
+
+// var num=4
+// var s_num=6
+
+// if(num!=s_num){
+//     alert("congratulation")
+// }
+
+// var num=50
+// if(num==50){
+//     alert("match")
+// }
+// else{
+//     alert("not match")
+// }
+
+// var marks=+prompt("enter marks")
+// var percentage=(marks*100)/500
+
+// if(percentage>=80){
+//     alert("A+")
+// }
+// else if(percentage>=70){
+//     alert("A")
+// }
+// else{
+//     alert("good")
+// }
+// var a=9
+// if (a === 10) {
+//     alert("a is 10");
+//     }
+//     else{
+//         alert("the correct value is "+a)
+//     }
+
+// var city=prompt("city name")
+// if(city=="karachi"){
+//     alert("this is"+city)
+// }
+// else if(city=="lahore"){
+//     alert("city is"+city)
+// }
+// else{
+//     alert("city is"+city)
+// }
+
+// if(a==b && c==d)
+// if(a==b || c==d)
+
+
+
+// if(name1=="hamza" || name2=="arsalan")
+
+// if(age<60)
+
+// var num1=5;
+// var num2=10;
+
+// if(num1<num2 || num1>num2){
+//     alert ("message")
+// }
+
+// var f_name=prompt("enter first name")
+// var l_name=prompt("enter last name")
+// var y_name="rohail"
+// var lastName="rizwan"
+// if (f_name==y_name && l_name==lastName){
+//     alert(f_name+l_name)
+// }
+
+// chap15
+
+// var password=prompt("")
+// if(password != "")
+// {
+//     if(password.length <= 5){
+//         alert("greater than 5")
+//     }
+//     else{
+//         alert("ok")
+//     }
+// }
+// else{
+//     alert("not")
+// }
+
+// var a=0
+// var c="Max"
+// if (a === 1) {
+//     if (c === "Max") {
+//     alert("OK");
+//     }
+//     }
+
+//     if (a !== 1) 
+//         if (c === "Max") {
+        
+//         alert("not equal");
+//         }
+//         }
+
+// var num1=5
+// var num2=4
+
+// if(num1!=num2){
+//     if(num1<num2){
+//         alert(num2)
+//     }
+//     else{
+//         alert(num1)
+//     }
+// }
+
+// var arr=[]
+
+// var arr=["rohail"]
+
+// var alphabet = ["h","i","j","k"]
+
+// var letter=alphabet[2]
+// alert(letter)
+
+// var alphabet=["h","i","j","k", "l","m", "n", "o"]
+// var l=alphabet.length
+// console.log(l)
+
+// var arr=["ali"]
+// arr.push("rehman")
+// alert(arr[1])
+
+// var Alphabet=["h","i","j","k"]
+// Alphabet.pop()
+// console.log(Alphabet)
+
+// var Alphabet=["h","i","j","k"]
+// Alphabet.push("l")
+// console.log(Alphabet)
+
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"]
+// sizes.shift()
+// console.log(sizes)
+
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"]
+// sizes.unshift(1,2,3)
+// console.log(sizes)
+
+// var arr=["2"]
+// arr.unshift("1")
+// console.log(arr[0])
+
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"]
+// sizes.splice(2,0,"l")
+// console.log(sizes)
+
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"]
+// var copy=sizes.slice(0,3)
+// console.log(copy)
+
+// var pets = ["dog", "cat", "ox", "duck", "frog"]
+// pets.splice(0,4,"cow","goat")
+// console.log(pets)
+// var pets = ["dog", "cat", "ox", "duck", "frog"]
+// pets.splice(1,3)
+// console.log(pets)
+
+// var pets = ["dog", "cat", "ox", "duck", "frog", "flea"]
+// pets.pop()
+// pets.splice(1,3)
+// console.log(pets)
+
+// for(vari=0;i<10;i++)
+
+// for (var i=1; i<=12;i++)
+
+// for var i = 0 i <= 4 i++
+// for (var i = 0 ; i <= 4 ; i++)
+
+// for (var count = 0; count < 100; count++) {
+
+//   }
+
+// for (var i = 3; i > 0; i--) {
+  
+//   }
+
+// var flag=true
+
+// var pets = ["dog", "cat", "ox", "duck", "frog", "flea"];
+
+// for (var i = 0; i < pets.length; i++) {
+
+// }
+
+// for (var i=0; i<10; i++){
+//     console.log(i)
+//     if(i==1){
+//         break
+//     }
+// }
+
+// var firstArr = ["a", "b", "c", "d", "e", "f"];
+// var secondArr = [1, 2, 3, 4, 5, 6];
+
+// for(var i=0; i<firstArr.length; i++){
+//     for(var j=0; j<secondArr.length;j++){
+//         console.log(firstArr[i] + secondArr[j] )
+//     }
+    
+// }
+// var userInput=5
+// var list=[2,4,5,8,10]
+// var matchFound = false;
+// for (var i = 0; i < list.length; i++) {
+// if (userInput === list[i]) {
+// alert("Match found");
+// matchFound = true;
+// break;
+// }
+// };
+
+// var arr=["ali","rehman"]
+// var user=prompt("enter name")
+// for (var i=0;i<arr.length;i++){
+//     if(user==arr[i]){
+//         alert(user)
+//         break
+//     }
+//     else{
+//         alert("please enter correct name")
+//     }
+// }
